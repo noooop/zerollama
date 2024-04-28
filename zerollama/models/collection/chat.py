5d99fa8ea008dc97d5daff50459ca1ca3276cff1
noooop <noooop@126.com>
2024-04-28 15:51:04 +0800

    add support llama3zh

--- zerollama/models/collection/chat.py
+++ zerollama/models/collection/chat.py
@@ -5,9 +5,10 @@ from zerollama.models.openbmb.minicpm import MiniCPM
 from zerollama.models.qwen.qwen1_5 import Qwen1_5
 from zerollama.models.yi.model import Yi
 from zerollama.models.llama.llama3 import Llama3
+from zerollama.models.llama.llama3zh import Llama3ZH
 
 
-CHAT_MODELS = [DeepSeekLLM, MiniCPM, Qwen1_5, Yi, Llama3]
+CHAT_MODELS = [DeepSeekLLM, MiniCPM, Qwen1_5, Yi, Llama3, Llama3ZH]
 CHAT_MODELS_NAME_MAP = dict()
 CHAT_MODELS_FAMILY_MAP = {m.family: m for m in CHAT_MODELS}
 
---
+++ zerollama/models/llama/llama3zh.py
@@ -0,0 +1,27 @@
+
+from zerollama.core.models.chat import ChatModel
+
+
+class Llama3ZH(ChatModel):
+    family = "llama3zh"
+    model_kwargs = {}
+    header = ["name", "size", "quantization", "bits"]
+    info = [
+        # name                                 size      quantization(_, GPTQ, AWQ)     bits
+        ["Unichat-llama3-Chinese-8B",          "8B",     "",                            ""],
+    ]
+
+
+if __name__ == '__main__':
+    import torch
+    from zerollama.inference_backend.hf_transformers.main import run_test
+
+    for model_name in ["Unichat-llama3-Chinese-8B"]:
+        run_test(model_name, stream=False)
+
+        print(torch.cuda.memory_allocated() / 1024 ** 2)
+
+    for model_name in ["Unichat-llama3-Chinese-8B"]:
+        run_test(model_name, stream=True)
+
+        print(torch.cuda.memory_allocated() / 1024 ** 2)
