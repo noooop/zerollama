66da9ff11401235a3f63e3c3cbfc5204191f4150
noooop <noooop@126.com>
2024-05-22 17:19:12 +0800

    v0.3 Releases

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="zerollama",
-    version="v0.2",
+    version="v0.3",
     author="noooop",
     author_email="noooop@live.cn",
     description="Microservices framework for large language model application linked by Zeromq",
