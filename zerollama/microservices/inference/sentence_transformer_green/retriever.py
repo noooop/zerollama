8e4911c65530d7939f598e058353d0a915f2ee30
noooop <noooop@126.com>
2024-06-06 15:43:50 +0800

    support matryoshka

--- zerollama/microservices/inference/sentence_transformer_green/retriever.py
+++ zerollama/microservices/inference/sentence_transformer_green/retriever.py
@@ -1,18 +1,17 @@
-import traceback
 
 import torch
 import requests
-from functools import partial
-from zerollama.core.config.main import config_setup
+from sklearn.preprocessing import normalize
 from zerollama.tasks.retriever.interface import RetrieverInterface
 from zerollama.tasks.retriever.protocol import RetrieverResponse
-from zerollama.tasks.retriever.collection import get_model_config_by_name, get_model_by_name
+from zerollama.tasks.retriever.collection import get_model_by_name
 from zerollama.tasks.base.download import get_pretrained_model_name_or_path
 
 
 class SentenceTransformerRetriever(RetrieverInterface):
     def __init__(self, model_name, local_files_only=True, device="cuda"):
-        model_config = get_model_config_by_name(model_name)
+        model = get_model_by_name(model_name)
+        model_config = model.get_model_config(model_name)
 
         if model_config is None:
             raise FileNotFoundError(f"model [{model_name}] not supported.")
@@ -25,8 +24,8 @@ class SentenceTransformerRetriever(RetrieverInterface):
         self.trust_remote_code = self.model_config.model_kwargs.get("trust_remote_code", False)
         self.pretrained_model_name_or_path = get_pretrained_model_name_or_path(model_name=model_name,
                                                                                local_files_only=local_files_only,
-                                                                               get_model_by_name=get_model_by_name,
-                                                                               get_model_config_by_name=get_model_config_by_name)
+                                                                               get_model_by_name=get_model_by_name)
+        self.matryoshka_support = self.model_info.get("matryoshka", False)
 
         self.model = None
         self.n_concurrent = 1
@@ -46,9 +45,15 @@ class SentenceTransformerRetriever(RetrieverInterface):
         options = options or {}
         normalize_embeddings = options.get("normalize_embeddings", True)
 
-        kwargs = {"normalize_embeddings": normalize_embeddings}
+        if self.matryoshka_support and "matryoshka_dim" in options:
+            matryoshka_dim = options["matryoshka_dim"]
+            dense_vecs = self.model.encode(sentences)
+            dense_vecs = dense_vecs[..., :matryoshka_dim]
+            dense_vecs = normalize(dense_vecs, norm="l2", axis=1)
+        else:
+            kwargs = {"normalize_embeddings": normalize_embeddings}
+            dense_vecs = self.model.encode(sentences, **kwargs)
 
-        dense_vecs = self.model.encode(sentences, **kwargs)
         return RetrieverResponse(model=self.model_name, vecs={"dense_vecs": dense_vecs})
 
     @property
