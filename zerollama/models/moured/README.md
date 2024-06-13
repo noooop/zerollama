e23e34ece9a88deb2c20f150f0d11035580282e8
noooop <noooop@126.com>
2024-06-13 10:30:11 +0800

    support moured/YOLOv10-Document-Layout-Analysis

---
+++ zerollama/models/moured/README.md
@@ -0,0 +1,37 @@
+# YOLOv10-Document-Layout-Analysis
+
+### setup
+
+1. 安装 依赖 
+```
+$ pip install -r zerollama\models\moured\requirements.txt  
+```
+
+4. 下载 模型
+想办法把下面6个模型下载下来
+下载地址 https://github.com/moured/YOLOv10-Document-Layout-Analysis/releases
+```
+yolov10b_best.pt
+yolov10l_best.pt
+yolov10m_best.pt
+yolov10n_best.pt
+yolov10s_best.pt
+yolov10x_best.pt
+```
+并放入与zerollama根目录 平级的 models/YOLOv10-Document-Layout-Analysis
+```
+- zerollama
+    - ......
+- models
+    - YOLOv10-Document-Layout-Analysis
+        - yolov10b_best.pt
+        - ....
+```
+
+### License
+MIT license
+
+### Reference
+[GITHUB](https://github.com/moured/YOLOv10-Document-Layout-Analysis)
+
+[huggingface](https://huggingface.co/omoured/YOLOv10-Document-Layout-Analysis)
---
+++ zerollama/models/moured/dla.py
@@ -0,0 +1,38 @@
+
+from zerollama.tasks.dla.interface import DLAModel
+
+
+class YOLOv10DocumentLayoutAnalysis(DLAModel):
+    family = "YOLOv10-Document-Layout-Analysis"
+    header = ["name", "weight_path"]
+    info = [
+        # name                                                weight_path
+        ["YOLOv10-Document-Layout-Analysis/yolov10b_best.pt", "yolov10b_best.pt"],
+        ["YOLOv10-Document-Layout-Analysis/yolov10l_best.pt", "yolov10l_best.pt"],
+        ["YOLOv10-Document-Layout-Analysis/yolov10m_best.pt", "yolov10m_best.pt"],
+        ["YOLOv10-Document-Layout-Analysis/yolov10n_best.pt", "yolov10n_best.pt"],
+        ["YOLOv10-Document-Layout-Analysis/yolov10s_best.pt", "yolov10s_best.pt"],
+        ["YOLOv10-Document-Layout-Analysis/yolov10x_best.pt", "yolov10x_best.pt"],
+    ]
+    inference_backend = "zerollama.models.moured.backend.dla:YOLOv10DocumentLayoutAnalysis"
+
+
+if __name__ == '__main__':
+    import os
+    import cv2
+    from pathlib import Path
+    from zerollama.tasks.dla.utils import get_annotated_image
+
+    dla_test_path = Path(os.path.dirname(__file__)).parent.parent.parent / "static/test_sample/dla"
+
+    input_path = dla_test_path / "input_sample.png"
+    img_BGR = cv2.imread(str(input_path))
+    img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+
+    for model_name in [x[0] for x in YOLOv10DocumentLayoutAnalysis.info]:
+        model = YOLOv10DocumentLayoutAnalysis.get_model(model_name)
+        model.load()
+        results = model.detection(img_RGB, options={"conf": 0.2, "iou": 0.8})
+
+        annotated_image = get_annotated_image(img_RGB, results)
+        cv2.imwrite(f'result-{model_name.replace("/", "-")}.jpg', annotated_image)
