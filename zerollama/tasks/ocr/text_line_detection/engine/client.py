85d6a1a206ccf0529b36707b5f105a4e4293ecac
noooop <noooop@126.com>
2024-06-14 18:23:16 +0800

    support text line detection

---
+++ zerollama/tasks/ocr/text_line_detection/engine/client.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+from zerollama.core.framework.nameserver.client import ZeroClient
+from zerollama.tasks.ocr.text_line_detection.protocol import PROTOCOL
+from zerollama.tasks.ocr.text_line_detection.protocol import TextLineDetectionRequest, TextDetectionResult
+
+CLIENT_VALIDATION = True
+
+
+class TLDClient(ZeroClient):
+    protocol = PROTOCOL
+
+    def __init__(self, nameserver_port=None):
+        ZeroClient.__init__(self, self.protocol, nameserver_port)
+
+    def detection(self, name, image, options=None):
+        method = "inference"
+        data = {"model": name,
+                "image": image,
+                "options": options or dict()}
+        if CLIENT_VALIDATION:
+            data = TextLineDetectionRequest(**data).dict()
+
+        rep = self.query(name, method, data)
+        if rep is None:
+            raise RuntimeError(f"DLA [{name}] server not found.")
+
+        if rep.state != "ok":
+            raise RuntimeError(f"DLA [{name}] error, with error msg [{rep.msg}]")
+
+        return TextDetectionResult(**rep.msg)
+
+
+if __name__ == '__main__':
+    import os
+    from PIL import Image
+    from pathlib import Path
+    from zerollama.tasks.ocr.text_line_detection.utils import get_annotated_image
+
+    dla_test_path = Path(os.path.dirname(__file__)).parent.parent.parent.parent.parent / "static/test_sample/dla"
+
+    input_path = dla_test_path / "input_sample.png"
+    image = Image.open(input_path)
+
+    model_name = "surya_tld"
+
+    client = TLDClient()
+    print("=" * 80)
+    print(f"Wait {model_name} available")
+    client.wait_service_available(model_name)
+    print(client.get_services(model_name))
+
+    print("=" * 80)
+    print('ZeroDLAInferenceEngine support_methods')
+    print(client.support_methods(model_name))
+    print(client.info(model_name))
+
+    results = client.detection(model_name, image)
+    annotated_image = get_annotated_image(image, results)
+    annotated_image.save(f'result-{model_name.replace("/", "-")}.jpg')
---
+++ zerollama/tasks/ocr/text_line_detection/protocol.py
@@ -0,0 +1,53 @@
+import copy
+from pydantic import BaseModel, ConfigDict, Field, ValidationError
+from pydantic import field_validator, computed_field
+from typing import Literal, Optional, List, Dict, Any, Union, Tuple
+
+from zerollama.core.framework.zero.protocol import (
+    ZeroServerRequest,
+    ZeroServerResponse,
+    ZeroServerResponseOk,
+    ZeroServerStreamResponseOk,
+    ZeroServerResponseError
+)
+
+ENGINE_CLASS = "zerollama.tasks.ocr.text_line_detection.engine.server:ZeroTLDInferenceEngine"
+MANAGER_NAME = "ZeroTLDInferenceManager"
+PROTOCOL = "text_line_detection"
+
+TLD_ENGINE_CLASS = ENGINE_CLASS
+TLD_MANAGER_NAME = MANAGER_NAME
+TLD_PROTOCOL = PROTOCOL
+
+
+class TextLineDetectionModelConfig(BaseModel):
+    name: str
+    info: dict
+    family: str
+    protocol: str = PROTOCOL
+    model_kwargs: dict
+
+    model_config = ConfigDict(
+        protected_namespaces=()
+    )
+
+
+class TextLineDetectionRequest(BaseModel):
+    image: Any
+    options: Optional[Dict] = None
+
+
+class Bbox(BaseModel):
+    bbox: List[float]
+    confidence: Optional[float] = None
+
+    @field_validator('bbox')
+    @classmethod
+    def check_4_elements(cls, v: List[float]) -> List[float]:
+        if len(v) != 4:
+            raise ValueError('bbox must have 4 elements')
+        return v
+
+
+class TextDetectionResult(BaseModel):
+    bboxes: List[Bbox]
