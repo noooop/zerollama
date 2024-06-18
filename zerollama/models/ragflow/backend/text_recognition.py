a41fa11d801802e8ab2f6e288b6a20f9dbfd9140
noooop <noooop@126.com>
2024-06-18 19:23:53 +0800

    ocr utils

--- zerollama/models/ragflow/backend/text_recognition.py
+++ zerollama/models/ragflow/backend/text_recognition.py
@@ -30,6 +30,8 @@ class DeepDocTextRecognition(TextRecognitionInterface):
         self.model = None
         self.drop_score = 0.5
         self.crop_image_res_index = 0
+        self.sorted_boxes = None
+        self.get_rotate_crop_image = None
         self.n_concurrent = 1
 
     def load(self):
@@ -39,69 +41,15 @@ class DeepDocTextRecognition(TextRecognitionInterface):
         import sys
         sys.path.append(str(ragflow_path))
         from deepdoc.vision.ocr import TextRecognizer, get_project_base_directory
+        from zerollama.tasks.ocr.utils import sorted_boxes, get_rotate_crop_image
 
         model_dir = os.path.join(
             get_project_base_directory(),
             "rag/res/deepdoc")
 
         self.model = TextRecognizer(model_dir)
-
-    def sorted_boxes(self, dt_boxes):
-        """
-        Sort text boxes in order from top to bottom, left to right
-        args:
-            dt_boxes(array):detected text boxes with shape [4, 2]
-        return:
-            sorted boxes(array) with shape [4, 2]
-        """
-        num_boxes = dt_boxes.shape[0]
-        sorted_boxes = sorted(dt_boxes, key=lambda x: (x[0][1], x[0][0]))
-        _boxes = list(sorted_boxes)
-
-        for i in range(num_boxes - 1):
-            for j in range(i, -1, -1):
-                if abs(_boxes[j + 1][0][1] - _boxes[j][0][1]) < 10 and \
-                        (_boxes[j + 1][0][0] < _boxes[j][0][0]):
-                    tmp = _boxes[j]
-                    _boxes[j] = _boxes[j + 1]
-                    _boxes[j + 1] = tmp
-                else:
-                    break
-        return _boxes
-
-    def get_rotate_crop_image(self, img, points):
-        '''
-        img_height, img_width = img.shape[0:2]
-        left = int(np.min(points[:, 0]))
-        right = int(np.max(points[:, 0]))
-        top = int(np.min(points[:, 1]))
-        bottom = int(np.max(points[:, 1]))
-        img_crop = img[top:bottom, left:right, :].copy()
-        points[:, 0] = points[:, 0] - left
-        points[:, 1] = points[:, 1] - top
-        '''
-        assert len(points) == 4, "shape of points must be 4*2"
-        img_crop_width = int(
-            max(
-                np.linalg.norm(points[0] - points[1]),
-                np.linalg.norm(points[2] - points[3])))
-        img_crop_height = int(
-            max(
-                np.linalg.norm(points[0] - points[3]),
-                np.linalg.norm(points[1] - points[2])))
-        pts_std = np.float32([[0, 0], [img_crop_width, 0],
-                              [img_crop_width, img_crop_height],
-                              [0, img_crop_height]])
-        M = cv2.getPerspectiveTransform(np.float32(points), pts_std)
-        dst_img = cv2.warpPerspective(
-            img,
-            M, (img_crop_width, img_crop_height),
-            borderMode=cv2.BORDER_REPLICATE,
-            flags=cv2.INTER_CUBIC)
-        dst_img_height, dst_img_width = dst_img.shape[0:2]
-        if dst_img_height * 1.0 / dst_img_width >= 1.5:
-            dst_img = np.rot90(dst_img)
-        return dst_img
+        self.sorted_boxes = sorted_boxes
+        self.get_rotate_crop_image = get_rotate_crop_image
 
     def recognition(self, image, lang, lines, options=None):
         options = options or {}
