4e7400d90db084febaad09100fde6d005f471d4a
noooop <noooop@126.com>
2024-06-29 14:11:12 +0800

    support 360LayoutAnalysis

--- zerollama/models/qihoo360/document_layout_analysis.py
+++ zerollama/models/qihoo360/document_layout_analysis.py
@@ -1,34 +1,36 @@
 
 from zerollama.tasks.ocr.document_layout_analysis.interface import DocumentLayoutAnalysisModel
+from zerollama.models.qihoo360.utils import paper_class_names, report_class_names, publaynet_class_names, general6_class_names
 
 
 class LayoutAnalysis360(DocumentLayoutAnalysisModel):
     family = "360LayoutAnalysis"
-    header = ["name", "weight_path"]
+    header = ["name", "weight_path", "class_names"]
     info = [
-        # name                             weight_path
-        ["360LayoutAnalysis/paper-8n.pt",  "paper-8n.pt"],
-        ["360LayoutAnalysis/report-8n.pt", "report-8n.pt"],
+        # name                                weight_path
+        ["360LayoutAnalysis/paper-8n.pt",     "paper-8n.pt",      paper_class_names],
+        ["360LayoutAnalysis/report-8n.pt",    "report-8n.pt",     report_class_names],
+        ["360LayoutAnalysis/publaynet-8n.pt", "publaynet-8n.pt",  publaynet_class_names],
+        ["360LayoutAnalysis/general6-8n.pt",  "general6-8n.pt",   general6_class_names],
     ]
     inference_backend = "zerollama.models.qihoo360.backend.document_layout_analysis:LayoutAnalysis360"
 
 
 if __name__ == '__main__':
     import os
-    import cv2
+    from PIL import Image
     from pathlib import Path
     from zerollama.tasks.ocr.document_layout_analysis.utils import get_annotated_image
 
     dla_test_path = Path(os.path.dirname(__file__)).parent.parent.parent / "static/test_sample/dla"
 
     input_path = dla_test_path / "input_sample.png"
-    img_BGR = cv2.imread(str(input_path))
-    img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+    image = Image.open(input_path)
 
     for model_name in [x[0] for x in LayoutAnalysis360.info]:
         model = LayoutAnalysis360.get_model(model_name)
         model.load()
-        results = model.detection(img_RGB, options={"conf": 0.5, "line_width": 2})
+        results = model.detection(image, options={"conf": 0.5, "line_width": 2})
 
-        annotated_image = get_annotated_image(img_RGB, results)
-        cv2.imwrite(f'result-{model_name.replace("/", "-")}.jpg', annotated_image)
+        annotated_image = get_annotated_image(image, results)
+        annotated_image.save(f'result-{model_name.replace("/", "-")}.jpg')
